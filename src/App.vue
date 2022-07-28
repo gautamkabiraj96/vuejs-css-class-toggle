4e7ea5ad160eda40d1e90fade366e26bb9e21cff
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2022-07-29 00:54:41 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <button @click="isDanger = !isDanger">Toggle danger</button>
+    <div
+      :class="[isDanger ? 'danger' : '', !isDanger ? 'safe' : '', 'large-text']"
+      style="margin-top: 15px"
+    >
+      {{ isDanger ? "Danger" : "Safe" }}
+    </div>
+    <div class="np-credits">wwww.nightprogrammer.com</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return { isDanger: false };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  padding: 24px;
+  font-size: 20px;
+}
+
+button {
+  background-color: #004cbe;
+  color: #ffffff;
+  border: 0px;
+  border-radius: 6px;
+  padding: 6px 20px;
+  margin-right: 1em;
+  cursor: pointer;
+  transition: all 0.3s;
+}
+
+button:hover {
+  background-color: #2f79e7;
+  transition: all 0.3s;
+}
+
+.np-credits {
+  font-size: 12px;
+  margin-top: 12px;
+  color: #4b4b4b;
+}
+
+.danger {
+  color: red;
+}
+
+.safe {
+  color: green;
+}
+
+.large-text {
+  font-size: 30px;
+}
+</style>
